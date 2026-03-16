bd5a2cc945f5a82dbf0627dbd77ab3cca783fb6e
AxelRodriguez-dot <23151212@aguascalientes.tecnm.mx>
2026-03-15 20:54:37 -0600

    actualizacion para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://AxelRodriguez-dot.github.io',
+  base: '/QR-CodeComponent',
   vite: {
     plugins: [tailwindcss()],
   },
